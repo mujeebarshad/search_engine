9873cfb7b487913d3ea47745d4bbe70669ada18e
mujeebarshad <mujeebarshad62@gmail.com>
2022-03-20 01:22:14 +0500

    handled spaces

--- search_engine.rb
+++ search_engine.rb
@@ -11,23 +11,23 @@ KILOMETER_IN_METERS = 1000
 MAX_DECIMAL_ROUND = 3
 
 def read_json_from_file(file_name)
-	file = File.open(file_name)
-	JSON.load(file)
+  file = File.open(file_name)
+  JSON.load(file)
 rescue StandardError => e
-	logger.info("Error loading json file! | reason: #{e.message}")
-	[]
+  logger.info("Error loading json file! | reason: #{e.message}")
+  []
 end
 
 def calculate_distance_between_geo_locations(first_geo_location, second_geo_location)
-	first_geo_location 	= first_geo_location.values
-	second_geo_location = second_geo_location.values
-	return -1 if first_geo_location == nil || second_geo_location == nil
+  first_geo_location 	= first_geo_location.values
+  second_geo_location = second_geo_location.values
+  return -1 if first_geo_location == nil || second_geo_location == nil
 
-	Geocoder::Calculations.distance_between(first_geo_location, second_geo_location, units: :km)
+  Geocoder::Calculations.distance_between(first_geo_location, second_geo_location, units: :km)
 end
 
 def distinguish_and_arrange_distance(distance)
-	if distance < 1
+  if distance < 1
     "#{(distance * KILOMETER_IN_METERS).round(MAX_DECIMAL_ROUND)}m"
   else
     "#{distance.round(MAX_DECIMAL_ROUND)}km"
@@ -35,31 +35,31 @@ def distinguish_and_arrange_distance(distance)
 end
 
 def create_json_result(results, total_data)
-	{
-		"totalHits": results.length,
+  {
+    "totalHits": results.length,
     "totalDocuments": total_data,
     "results": results
-	}.to_json
+  }.to_json
 end
 
 def search_engine(service_name, geo_location) # params => string, hash {lat, long}
-	location_of_services = read_json_from_file(FILE_NAME)
-	result = []
-	total_locations = location_of_services.length
-	return create_json_result(result, total_locations) if service_name.empty? || geo_location.empty? # in case empty data we return 0 hits
+  location_of_services = read_json_from_file(FILE_NAME)
+  result = []
+  total_locations = location_of_services.length
+  return create_json_result(result, total_locations) if service_name.empty? || geo_location.empty? # in case empty data we return 0 hits
 
-	location_of_services.each do |service_location|
-		similarity_score = String::Similarity.cosine(service_location['name'], service_name) # string difference score
-		next if similarity_score < SIMILARITY_THRESHOLD
+  location_of_services.each do |service_location|
+    similarity_score = String::Similarity.cosine(service_location['name'], service_name) # string difference score
+    next if similarity_score < SIMILARITY_THRESHOLD
 
-		distance_between_locations = calculate_distance_between_geo_locations(geo_location, service_location['position'])
-		next if distance_between_locations == -1
+    distance_between_locations = calculate_distance_between_geo_locations(geo_location, service_location['position'])
+    next if distance_between_locations == -1
 
-		service_location['distance'] = distinguish_and_arrange_distance(distance_between_locations)
-		service_location['score'] 	 = similarity_score
-		result.push(service_location)
-	end
-	create_json_result(result, total_locations)
+    service_location['distance'] = distinguish_and_arrange_distance(distance_between_locations)
+    service_location['score'] 	 = similarity_score
+    result.push(service_location)
+  end
+  create_json_result(result, total_locations)
 end
 
 puts "Serivce Name: "
